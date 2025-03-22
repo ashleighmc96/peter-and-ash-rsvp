e56121a6a1ce4e102028468b187a6cafab0e014b
ashleighmc96 <ashleigh.mc96@gmail.com>
2025-03-22 18:45:52 +1100

    Add files via upload

--- main.jsx
+++ main.jsx
@@ -1,9 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import EngagementRSVP from './App';
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import EngagementRSVP from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <EngagementRSVP />
   </React.StrictMode>
-);
+)
